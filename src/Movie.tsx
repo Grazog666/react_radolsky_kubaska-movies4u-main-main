d2fabbf8eec5abc77e781bfb56cf904c2e7c65de
z20ist11 <z20ist11@z00p3-12.SPSKNM>
2022-12-13 10:00:27 +0100

    fds

--- src/Movie.tsx
+++ src/Movie.tsx
@@ -1,9 +1,16 @@
+
+
+
+
 import { useState } from "react"
 export default function Movie()
 {
     const [title, setTitle] = useState("")
     const [img, setImg] = useState("")
+    
+
     return(
+        
         <div className="movie">
             <img src={img} alt="image" className="imgofmovie"/>
                 
